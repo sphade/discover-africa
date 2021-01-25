852b1108a65faa53437fe3f4e626ff9a2910a186
lawalgit <lawal@gmai.com>
2021-01-25 07:23:42 -1200

    created the Human object

---
+++ script.js
@@ -0,0 +1,16 @@
+Human = {
+    name:  {
+        firstName: 'lawal',
+        lastName:'adebola',
+    },
+    age: 17,
+    height: '14 metters',
+    gender:"male",
+    complete() {
+        return  this.name.firstName + this.name.lastName + 'is a '+this.gender
+    }
+
+}
+
+console.log(Human.complete())
+
